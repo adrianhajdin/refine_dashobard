5636605eddd99cac04c99c052e1cc10dc0918f55
adrianhajdin <adrianhajdin00@gmail.com>
2023-02-04 12:58:29 +0100

    ...

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -56,7 +56,7 @@ const App = () => {
 
       // Save user to MongoDB
       if (profileObj) {
-        const response = await fetch('http://localhost:8080/api/v1/users', {
+        const response = await fetch('https://yariga.up.railway.app/api/v1/users', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({
@@ -122,7 +122,7 @@ const App = () => {
       <GlobalStyles styles={{ html: { WebkitFontSmoothing: 'auto' } }} />
       <RefineSnackbarProvider>
         <Refine
-          dataProvider={dataProvider('http://localhost:8080/api/v1')}
+          dataProvider={dataProvider('https://yariga.up.railway.app/api/v1')}
           notificationProvider={notificationProvider}
           ReadyPage={ReadyPage}
           catchAll={<ErrorComponent />}
